adcafed332bf7ccf0b2d6516771dca82fe99f451
Eric Zhu <eric@ericz.me>
2023-03-27 01:44:10 -0400

    fix(auto/http): ptr scoping

--- internal/auto/http.go
+++ internal/auto/http.go
@@ -12,11 +12,18 @@ import (
 func (a *Auto) httpReq(method string, urlStr string, data any) (respBodyBytes []byte, status int, err error) {
 	c := a.getHttpsClient()
 
-	resp := &http.Response{}
-
 	switch method {
 	case "GET":
-		resp, err = c.Get(urlStr)
+		resp, err := c.Get(urlStr)
+		if err != nil {
+			return nil, -1, err
+		}
+		if resp.Body == nil {
+			return nil, -1, errors.New("response body is nil")
+		}
+		defer resp.Body.Close()
+		respBodyBytes, err = ioutil.ReadAll(resp.Body)
+		status = resp.StatusCode
 	case "POST", "PUT", "PATCH":
 		if data == nil {
 			return nil, -1, errors.New("data is nil")
@@ -41,15 +48,16 @@ func (a *Auto) httpReq(method string, urlStr string, data any) (respBodyBytes []
 			Body: ioutil.NopCloser(bytes.NewBuffer(reqBodyBytes)),
 		}
 
-		resp, err = c.Do(&req)
+		resp, err := c.Do(&req)
+		if resp.Body == nil {
+			return nil, -1, errors.New("response body is nil")
+		}
+		defer resp.Body.Close()
+		respBodyBytes, err = ioutil.ReadAll(resp.Body)
+		status = resp.StatusCode
+	default:
+		return nil, -1, errors.New("invalid method")
 	}
 
-	if resp.Body == nil {
-		return nil, -1, errors.New("response body is nil")
-	}
-	defer resp.Body.Close()
-
-	respBodyBytes, err = ioutil.ReadAll(resp.Body)
-	status = resp.StatusCode
 	return
 }
